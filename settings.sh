8800ee4eb6d67d06cf53f7e83b044a9034830125
Subin <39542978+subeans@users.noreply.github.com>
2021-06-02 13:17:49 +0900

    for rnn profiling

--- settings.sh
+++ settings.sh
@@ -6,9 +6,13 @@ ln -s /usr/local/cuda-11.0 /usr/local/cuda
 
 # Upgrade pip3 and install tensorflow, tensorboard, dataset packages
 pip3 install --upgrade pip
-pip3 install tf-nightly
-pip3 install tb-nightly
+pip3 install tensorflow
+# pip3 install tf-nightly
+# pip3 install tb-nightly
 pip3 install tensorboard-plugin-profile
 pip3 install tensorflow_datasets
-pip3 install tesorflow-text-nightly
+# for bert model 
+pip3 install tesorflow-text
+pip3 install tensorflow-hub
+pip3 install tf-models-official
 mkdir tensorstats
